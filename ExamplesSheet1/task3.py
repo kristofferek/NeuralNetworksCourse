0b4cf26a36ef149844c400bb432bc17add9f8c6a
Kristoffer Ek <inzeer@gmail.com>
2017-09-18 21:45:23 +0200

    Improved the energy function

--- ExamplesSheet1/task3.py
+++ ExamplesSheet1/task3.py
@@ -17,12 +17,12 @@ class Backpropagation:
 					if counter < 2:
 						temp.append(float(word))
 					else:
-						temp = np.asarray(temp).T
+						temp = np.asarray(temp)
 						self.inputData.append(temp)
 						self.targetOutput.append(float(word))
 					counter += 1
-		np.asarray(self.inputData)
-		np.asarray(self.targetOutput)
+		self.inputData = np.asarray(self.inputData)
+		self.targetOutput = np.asarray(self.targetOutput)
 
 
 	def initValues(self, learnR, beta):
@@ -30,7 +30,6 @@ class Backpropagation:
 		self.beta = beta
 		self.weights = (0.2 - (-0.2)) * np.random.random_sample((2,1)) + (-0.2)
 		self.threshold = (1 - (-1)) * np.random.random_sample() + (-1)
-		print(self.weights)
 
 
 	def actFunc(self, b, deriv=False):
@@ -44,11 +43,7 @@ class Backpropagation:
 		else:
 			rand = randint(0,len(self.inputData)-1)
 		# Forward propagation
-		l0 = np.zeros((2,1))
-		y = 0
-		for x in self.inputData[rand]:
-			l0.itemset((y,0),x)
-			y+=1
+		l0 = (self.inputData[rand])[np.newaxis].T
 
 		b = np.dot(np.transpose(self.weights), l0) - self.threshold
 		l1 = self.actFunc(b)
@@ -63,23 +58,16 @@ class Backpropagation:
 
 		# Delta error
 		l1Delta = l1Error * self.actFunc(l1, True)
-
 		self.weights = np.add(self.weights, self.learnR * np.dot(l0, l1Delta))
 		self.threshold = np.add(self.threshold, -(self.learnR * l1Delta))
 
-	def energyShit(self):
-		sum = 0
-		for x in range(0, len(self.inputData)):
-			# Forward propagation
-			l0 = np.zeros((2,1))
-			y = 0
-			for z in self.inputData[x]:
-				l0.itemset((y,0),z)
-				y+=1
+	def energyFunc(self):
+		# Forward propagation
+		b = np.dot(self.inputData, self.weights) - self.threshold
 
-			b = np.dot(np.transpose(self.weights), l0) - self.threshold
-			output = self.actFunc(b)
-			sum += (self.targetOutput[x] - output)**2
+		output = self.actFunc(b)
+
+		sum = np.sum(np.power((self.targetOutput - output), 2))
 		return sum/2
 
 learnR = 0.02
@@ -91,7 +79,7 @@ back.readData()
 back.initValues(learnR, beta)
 for x in range(1,100000):
 	back.trainNetwork()
-	shitPlot.append(back.energyShit())
+	shitPlot.append(back.energyFunc())
 	if x % 10000 == 0:
 		print(x)
 back.trainNetwork(True)
